1d4d015b163893be2c802f10f1225072a8431374
SerhiiMaltsev <sm5zj@virginia.edu>
2022-04-21 03:57:39 -0400

    final version

--- trivia/src/components/Main.js
+++ trivia/src/components/Main.js
@@ -14,14 +14,60 @@ function Main() {
       })
   }, [url])
 
-  console.log(questions);
-
   if (questions){
+    var listOfQuestions = []
+
+    {Object.entries(questions.results).map(([key, value]) => (
+      listOfQuestions.push([value.question, value.correct_answer, value.incorrect_answers])
+    ))}
+
+    var answersFormatted = []
+
+    {Object.entries(listOfQuestions).map(([key, value]) => (
+      answersFormatted.push([value[0]])
+    ))}
+
+    var answersNotShuffled = []
+
+    for (var i in listOfQuestions){
+      for (var j in listOfQuestions[i][2]){
+        listOfQuestions[i].push(listOfQuestions[i][2][j])
+      }
+    }
+
+    for (var i in listOfQuestions){
+      listOfQuestions[i].splice(2,1);
+    }
+
+    var temp = []
+
+    for (var i in listOfQuestions){
+      temp = [[listOfQuestions[i][1], "true"]]
+      for (var j = 2; j<listOfQuestions[i].length; j++){
+        temp.push([listOfQuestions[i][j], "false" + String(j)])
+      }
+      listOfQuestions[i][1] = temp;
+    }
+
+    for (var k in listOfQuestions){
+      for (var i = listOfQuestions[k][1].length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = listOfQuestions[k][1][i];
+        listOfQuestions[k][1][i] = listOfQuestions[k][1][j];
+        listOfQuestions[k][1][j] = temp;
+      }
+    }
+
+    const dictOfQuestions = {}
+    dictOfQuestions["results"] = listOfQuestions;
+
+    console.log(dictOfQuestions.results)
+
     return (
         <div className="Main">
           <h1> Trivia </h1>
-          {Object.entries(questions.results).map(([key, value]) => (
-            <Question item = {value}/>
+          {Object.entries(dictOfQuestions.results).map(([key, value]) => (
+            <Question questiontext = {value[0]} allanswers = {value[1]}/>
           ))}
         </div>
     )
--- trivia/src/components/Question.js
+++ trivia/src/components/Question.js
@@ -1,46 +1,70 @@
 import './Question.css';
-import Answer from './Answer.js'
+import Answer from './Answer.js';
+import { Button, Radio, FormControl, FormLabel, RadioGroup, FormControlLabel, FormHelperText } from '@material-ui/core';
+import React, { useState } from "react";
 
 function Question(props) {
-  var answers = {};
 
-  {Object.entries(props.item.incorrect_answers).map(([key, value]) => (
-    answers[value] = false
-  ))}
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(false);
+  const [helperText, setHelperText] = useState('Choose wisely');
 
-  answers[props.item.correct_answer] = true
-  var arrayForShuffle = []
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target).value);
+    setHelperText(' ');
+    setError(false);
+  };
 
-  {Object.entries(answers).map(([key, value]) => (
-    arrayForShuffle.push([key, value])
-  ))}
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+    console.log(value)
+    if (value === "true") {
+      setHelperText('You got it!');
+      setError(false);
+    } else if (value === "false2") {
+      setHelperText('Sorry, wrong answer!');
+      setError(true);
+    } else if (value === "false3") {
+      setHelperText('Sorry, wrong answer!');
+      setError(true);
+    } else if (value === "false4") {
+      setHelperText('Sorry, wrong answer!');
+      setError(true);
+    } else {
+      setHelperText('Please select an option.');
+      setError(true);
+    }
+  };
 
-  for (var i = arrayForShuffle.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = arrayForShuffle[i];
-        arrayForShuffle[i] = arrayForShuffle[j];
-        arrayForShuffle[j] = temp;
-  }
-
-  answers = {}
-
-  for (var i = arrayForShuffle.length - 1; i >= 0; i--) {
-    answers[arrayForShuffle[i][0]] = arrayForShuffle[i][1];
-  }
-
-  var badQuestion = props.item.question;
+  var badQuestion = props.questiontext;
   var question = badQuestion.replace(/&quot;/g,'"');
   question = question.replace(/&#039;/g,'\'');
   question = question.replace(/&amp;/g,'\&');
 
-  console.log(question)
-
   return (
     <div className="Question">
       <h2> {question} </h2>
-      {Object.entries(answers).map(([key, value]) => (
-        <Answer text = {key} color = {value}/>
-      ))}
+      <form onSubmit={handleSubmit}>
+        <FormControl sx={{ m: 3 }} error={error} variant="standard">
+          <FormLabel id="demo-error-radios"></FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-error-radios"
+            name="quiz"
+            value={value}
+            onChange={handleRadioChange}
+          >
+          {Object.entries(props.allanswers).map(([value]) => (
+            <FormControlLabel value={props.allanswers[value][1]} control={<Radio />} label={props.allanswers[value][0]} />
+          ))}
+          </RadioGroup>
+          <FormHelperText>{helperText}</FormHelperText>
+          <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="outlined">
+            Check Answer
+          </Button>
+        </FormControl>
+      </form>
+
+
     </div>
   );
 }
